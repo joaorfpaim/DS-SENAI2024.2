b8fcb94404c4a9fc5303ec7e828bb7f8fc00479b
João Paim <162460902+joaorfpaim@users.noreply.github.com>
2024-08-21 10:43:31 -0300

    Update q4.py

--- aula 8/q4.py
+++ aula 8/q4.py
@@ -2,8 +2,11 @@ import os
 os.system('cls')
 
 nomes = []
-for x in range(5):
-    nomes.append(input(f'Digite o {x+1}° nome: '))
+for i in range(5):
+    nomes.append(input(f'Digite o {i+1}° nome: '))
     os.system('cls')
 nomes.sort()
-print(f'Os nomes em ordem alfabetica são {nomes}.')
+
+print('Os nomes em ordem alfabetica são:')
+for i in nomes:
+    print(i)
