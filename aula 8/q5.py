c401ffc091786653f2cdcb6f11507d2f7c4a9bfc
João Paim <162460902+joaorfpaim@users.noreply.github.com>
2024-08-21 10:55:28 -0300

    Update q5.py

--- aula 8/q5.py
+++ aula 8/q5.py
@@ -1,7 +1,15 @@
+import os
+import time
+
+os.system('cls')
+
 numeros = []
 for x in range(10):
-    numeros.append(int(input('Digite 10 números inteiros:')))
+    numeros.append(int(input(f'Digite {1+x}/10 números inteiros: ')))
+    os.system('cls')
     
 media = sum(numeros)/len(numeros)
+soma = sum(numeros)
+time.sleep(0.4)
 
-print(f'A média dos números é {media}')
+print(f'A soma dos números é {soma}, já a média dos é {media}')
